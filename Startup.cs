5de9f9100711aa9c9d82c1dfe87bcba9cb73d890
Gabriel L Martinez <g.lorenzo.martinez@gmail.com>
2018-12-05 14:30:43 -0400

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -49,22 +49,6 @@ namespace konoha
                 facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
             });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
-            .AddRazorPagesOptions(options =>
-            {
-                options.AllowAreas = true;
-                options.Conventions.AuthorizeAreaFolder("Identity", "/Account/Manage");
-                options.Conventions.AuthorizeAreaPage("Identity", "/Account/Logout");
-            });
-
-            services.ConfigureApplicationCookie(options =>
-            {
-                options.LoginPath = $"/Identity/Account/Login";
-                options.LogoutPath = $"/Identity/Account/Logout";
-                options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
-            });
-
             // services.AddSingleton<IEmailSender, EmailSender>();
             // services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
