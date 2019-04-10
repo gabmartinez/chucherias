94c331800eeca8a678c460dd08485e8925c3059e
Gabriel L Martinez <g.lorenzo.martinez@gmail.com>
2019-04-10 07:28:27 -0400

    Temporary fix the issue No coercion operator is defined between types 'System.Int16' and 'System.Boolean'

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -4,6 +4,8 @@ using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using chucherias.Models;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.AspNetCore.Identity;
 
 namespace chucherias.Data
 {
@@ -16,5 +18,25 @@ namespace chucherias.Data
         public DbSet<Category> Category { get; set; }
         public DbSet<Post> Post { get; set; }
         public DbSet<PostImage> PostImage { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Post>()
+                .Property(b => b.IsActive)
+                .HasConversion(new BoolToZeroOneConverter<Int16>());
+            builder.Entity<IdentityUser>()
+                .Property(b => b.EmailConfirmed)
+                .HasConversion(new BoolToZeroOneConverter<Int16>());
+            builder.Entity<IdentityUser>()
+                .Property(b => b.LockoutEnabled)
+                .HasConversion(new BoolToZeroOneConverter<Int16>());
+            builder.Entity<IdentityUser>()
+                .Property(b => b.PhoneNumberConfirmed)
+                .HasConversion(new BoolToZeroOneConverter<Int16>());
+            builder.Entity<IdentityUser>()
+                .Property(b => b.TwoFactorEnabled)
+                .HasConversion(new BoolToZeroOneConverter<Int16>());
+        }
     }
 }
